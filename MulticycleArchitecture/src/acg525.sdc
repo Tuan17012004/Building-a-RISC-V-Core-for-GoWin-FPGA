36cb7db1efe218cff27a9d02617da9f84cdbe299
tu4n_nguyen <146950486+Tuan17012004@users.noreply.github.com>
2026-06-12 04:44:22 +0700

    Delete MulticycleArchitecture directory

--- MulticycleArchitecture/src/acg525.sdc
+++
@@ -1,3 +0,0 @@
-# Timing Constraints - GoWIN EDA
-# Board ACG525 - Clock 50MHz tai pin T9
-create_clock -name clk -period 20 -waveform {0 10} [get_ports {clk}]
--- MulticycleArchitecture/src/flopr.sv
+++
@@ -1,9 +0,0 @@
-module flopr #(parameter WIDTH = 8) (
-    input              clk, reset,
-    input  [WIDTH-1:0] d,
-    output reg [WIDTH-1:0] q
-);
-    always @(posedge clk or posedge reset)
-        if (reset) q <= {WIDTH{1'b0}};
-        else       q <= d;
-endmodule
